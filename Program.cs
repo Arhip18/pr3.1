670c25e0aca3bb8f9e93070194ed0ff474cf6706
Yaroslav Nyanyushkin <108981413+MrMitrum@users.noreply.github.com>
2024-05-11 08:56:24 +0400

    plus

--- Program.cs
+++ Program.cs
@@ -10,8 +10,21 @@ namespace ConsoleApp_пр_3
     {
         static void Main(string[] args)
         {
-         
-            
+            int a;
+            int b;
+            int c;
+            char znak;
+            Console.WriteLine("Введите первую переменную: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите вторую переменную: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знак:");
+            znak = Convert.ToChar(Console.ReadLine());
+            if (znak == '+')
+            {
+                c = a + b;
+                Console.WriteLine("Ответ: " + c);
+            }
         }
     }
 }
